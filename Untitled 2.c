4282ab4bba44effa37384a760c6379478ad98db5
20019181 <161727068+20019181@users.noreply.github.com>
2024-05-01 17:05:08 +0200

    Add files via upload

---
+++ Untitled 2.c
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmaths>
+
+using namespace std;
+
+void euclidean distance (double x1, doubley1 double x2 double y2)
+double x= x2-x1
+double y= y2-y1
+ double dist =(sqrt(pow(x2-x1)2)+(pow(y2-y1)2))
+int main ()
+{
+  int coeffof x1
+  int coeffof x2
+  int coeffof y1
+  int coeffof y2
+  int distance btwn p1 and p2
+
+  cout<<"int coeffof x1"<<
+  cin>>8;
+  cout<<endl;
+
+  cout<<"int coeffof y1"<<
+  cin>>5;
+  cout<< endl;
+
+  cout>>"int coeffof x2"<<
+  cin>>4;
+  cout<<endl;
+
+  cout>>"int coeffof y2"<<
+  cin>>2;
+  cout<<endl;
+
+  cout<<"the euclidean distance between two point(p1 and p2)is"
+  cin>>5;
+
+  return 0
